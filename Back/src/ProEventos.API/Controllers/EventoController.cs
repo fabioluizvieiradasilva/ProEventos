b85b3c8964a69c74c9228b71e7c3c44333939dae
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2021-05-27 02:19:06 -0300

    Iniciando o EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,28 +1,37 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventoController: ControllerBase
+    public class EventoController : ControllerBase
     {
-        public EventoController(){}
+        private readonly DataContext _context;
+        public EventoController(DataContext context)
+        {
+            _context = context;
+
+        }
+
 
         [HttpGet]
-        public Evento Get()
+        [Route("")]
+        public IEnumerable<Evento> Get()
         {
-            return new Evento(){
-                EventoId = 1,
-                Tema = "Angular 12 e .NET 5",
-                Local = "Volta Redonda/RJ",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "foto.png"
-            };
+            return _context.Eventos;
         }
-        
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+        }
+
     }
 }
