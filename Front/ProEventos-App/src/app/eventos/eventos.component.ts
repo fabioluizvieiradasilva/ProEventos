0bf79ff13589ca5098b8211d714638ce9831a190
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2021-06-16 01:03:50 -0300

    Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public filteredEvents: any = [];
+
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string){
+    this._filterList = value;
+    this.filteredEvents = this.filterList ? this.filterEvents(this.filterList) : this.eventos;
+  }
+
+  filterEvents(value: string): any{
+    value = value.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(value) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(value) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -17,10 +40,17 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
-      error => console.log(error),
+      this.http.get('https://localhost:5001/api/evento').subscribe(
+      response => {
+        this.eventos = response;
+        this.filteredEvents = this.eventos;
+      },
+      error => console.log(error)
       );
+  }
+
+  public showImage(){
+    this.showImg = !this.showImg
   }
 
 }
